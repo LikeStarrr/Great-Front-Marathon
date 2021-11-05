85530b0750ca0c059bf47dfdd67eb14fd994bb8d
LikeStarrr <irok8888@mail.ru>
2021-11-05 21:15:15 +0300

    Calc vs Objects #21

--- CalcVsObj/script.js
+++ CalcVsObj/script.js
@@ -4,37 +4,36 @@
 //let inputOperator = prompt('Введите название операции');
 function checkData(firstOperand, secondOperand) {
     if ( (typeof firstOperand !== "number") || (typeof secondOperand !== "number") || secondOperand === 0) {
-        console.log('Data error!');
         return false;
-    }
-    
+    }    
     return true;
 }
 
 function Calculator(firstOperand, secondOperand, inputOperator) {
     if ( checkData(firstOperand, secondOperand) ){
         let operator = {
-            "add": firstOperand + secondOperand,
+            "sum": firstOperand + secondOperand,
             "sub": firstOperand - secondOperand,
             "multi": firstOperand * secondOperand,
             "div": firstOperand / secondOperand,
             "pow": firstOperand ** secondOperand,
-            "remainder": firstOperand % secondOperand,
+            "rem": firstOperand % secondOperand,
         }
         if (inputOperator in operator) {
             return operator[inputOperator];
         } else {
-        console.log('Unknown operation');
+        return "Unknown operation";
         }    
+    } else {
+        return "Data error!";    
     }
-
-    
+       
 } 
 console.log('Start');
-console.log(Calculator(2,3,'add'));
+console.log(Calculator(2,3,'sum'));
 console.log(Calculator(2,0,'div'));
-console.log(Calculator(2,'','add'));
+console.log(Calculator(2,'','sum'));
 console.log(Calculator(2,3,''));
-console.log(Calculator('2',3,'add'));
-console.log(Calculator(2,3,'add'));
-console.log(Calculator(2,3,'add'));
+console.log(Calculator('f',3,'sum'));
+console.log(Calculator(2,-1,'div'));
+console.log(Calculator(5,2,'rem'));
