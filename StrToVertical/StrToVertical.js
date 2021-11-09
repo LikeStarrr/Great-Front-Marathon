5b259255bb6e16470595c228c9e590cf95cf9f88
LikeStarrr <irok8888@mail.ru>
2021-11-10 00:33:05 +0300

    Change Str To Vertical

--- StrToVertical/StrToVertical.js
+++ StrToVertical/StrToVertical.js
@@ -1,5 +1,6 @@
 function ChangeStrToVertical(str){
-    if (str){
+    if (typeof(str)!== 'string') return console.log('введите строку!');
+    if (str.length){
         str[0] === 'м' ? console.log(str[0].toUpperCase()) : console.log(str[0])     
         str.length > 10 ? (str = str.slice(1,10)) : (str = str.slice(1, str.length))
         for (let char of str){
@@ -13,7 +14,12 @@ function ChangeStrToVertical(str){
 
 //ChangeStrToVertical('марафончикчикчик');
 //ChangeStrToVertical('железобетонный');
-ChangeStrToVertical('мамамамамамамамама');
+//ChangeStrToVertical('мамамамамамамамама');
 //ChangeStrToVertical('');
-//ChangeStrToVertical('ммм');
+ChangeStrToVertical('ммм');
+//Ошибки
+//ChangeStrToVertical('');
+//ChangeStrToVertical(NaN);
+//ChangeStrToVertical(undefined);
+//ChangeStrToVertical(true);
 
