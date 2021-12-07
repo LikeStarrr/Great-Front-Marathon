82ce474f7a3794f165809a480f3bc7e89127f906
LikeStarrr <irok8888@mail.ru>
2021-12-07 23:54:13 +0300

    try_catch

--- ToDoUI/main.js
+++ ToDoUI/main.js
@@ -2,25 +2,34 @@ import {PRIORITY_BLOCKS, ADD} from './view.js';
 
 for (let i = 0; i < ADD.BTNS.length; i++) {
 	ADD.BTNS[i].addEventListener ('click', function () {
-  	alert("Step2");
+ 
     let isAddContent = ADD.INPUTS[i].value.trim();
-    alert("Step3");
-    if (!isAddContent) return;
-    alert("Step4");
+    try{
+       if (!isAddContent) {
+         throw "err1";
+       }
+    } catch(error) {
+      if (error=="err1") {
+        alert("Enter new task!");
+        return;
+      }
+    }
+    
+ 
     const DIV = document.createElement('div');
     DIV.className = 'task';
-    alert("Step5");
+
     DIV.innerHTML = `<label> 
         <input class="task_check" type="checkbox"> 
         <span class="cursor"></span>
       </label>
       <span class="task_text" > ${ADD.INPUTS[i].value} </span>
       <button class="task_delete"><img src="./img/delete-task-icon.svg" alt="-"></button>`	
-      alert("Step6");
+ 
       PRIORITY_BLOCKS[i].append(DIV);
-      alert("Step7");
+
     ADD.INPUTS[i].value = '';
-    alert("Step8");
+
     DIV.querySelector('.task_check').addEventListener('click', changeColor);
     DIV.querySelector('.task_delete').addEventListener('click', deleteTask); 
   })
